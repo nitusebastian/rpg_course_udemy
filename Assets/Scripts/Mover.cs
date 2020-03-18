af2e71c1ff983033e43dd85b4482e18a80fb1a74
Sebastian2485 <nitusebastian85@yahoo.ro>
2020-03-18 12:44:15 +0200

    Finished Player basic movement and movement animations

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,6 +8,12 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
     private NavMeshAgent agent;
+
+    private Ray ray;
+
+    private Animator animator;
+    [SerializeField] private float maxSpeed = 50f;
+
     
     // Start is called before the first frame update
     void Start()
@@ -18,11 +24,37 @@ public class Mover : MonoBehaviour
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+
+    private void MoveToCursor()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            agent.SetDestination(hit.point);
+        }
     }
 }
