5177b8e3e68a10515bb45e8460773abbb528fd99
Sebastian2485 <nitusebastian85@yahoo.ro>
2020-03-24 16:14:07 +0200

    Added second scene

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RGP.Cinematics
+{
+    
+    
+    public class CinematicTrigger
+        : MonoBehaviour
+    {
+        private bool triggeredOnce = false;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!triggeredOnce && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                triggeredOnce = true;
+            }
+        }
+    }
+}
